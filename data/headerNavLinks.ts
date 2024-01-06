c2b52fb19fa69720a29303ee618332fa80f88c4b
artajohan369 <artajohan369@gmail.com>
2024-01-06 19:29:58 +0400

    in data/headerNavLinks.ts:2 changed  to

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,5 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
+  { href: '/', title: 'Blog' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
