7874c7dc9335448bf960441436e3134f7f862f0b
artajohan369 <155809127+artajohan369@users.noreply.github.com>
2024-02-13 23:12:14 +0400

    changed /data/siteMetaData.js:3

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Arta Muses',
   author: 'Arta Johan',
   headerTitle: 'Arta Muses',
   description: 'Personal blog of Arta Johan',
